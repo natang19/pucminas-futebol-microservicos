e1470c0348b6b4e5c29c6c33c4cdc4505ee9e4d9
Natan Guedes de Sa Cicero <natan.guedes198@outlook.com>
2022-03-22 13:21:21 -0300

    Setup inicial do projeto

---
+++ src/pucminas.futebol.jogadores.worker/Program.cs
@@ -0,0 +1,10 @@
+using pucminas.futebol.jogadores.worker;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
